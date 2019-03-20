0a415aaa400d911b33abc81e3f3a26a833dd1918
Karandeep Singh <karan.redrock@gmail.com>
2019-03-20 18:03:22 +0530

    Patient and doctor ddls added

---
+++ table ddls.sql
@@ -0,0 +1,12 @@
+create table doctor(
+name varchar(20),
+id varchar(30),
+age int,
+salary float
+);
+
+create table patient(
+name varchar(30),
+insurance_type varchar(40),
+dateoflastvisit datetime2
+);
