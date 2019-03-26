73510da06f10ef69d3a3a98c13e591c924481705
Karandeep Singh <karan.redrock@gmail.com>
2019-03-27 02:51:28 +0530

    Updates in ADF pipelines

--- Azure Data movement.py
+++ Azure Data movement.py
@@ -3,3 +3,7 @@ def pipeline("CopyRX30 Data"):
 	def activity("Copy"):
 		add.Source("linktoRX30")
 		add.Sink("insightsdwh")
+def pipeline("SingleShotSTG"):
+	def activity("lookupconfigdata"):
+
+	def activity("ForEachTable"):
--- SAM pipelines.py
+++ SAM pipelines.py
@@ -1,6 +1,8 @@
-#This is a Test file to start work on Git repo form second account
 
 def pipeline("CopySAMData"):
 	def createLinkedService("SAMApp"):
 		connection strin = '10.0.0.11'
 
+def pipeline("SAMInfustionAPP"):
+	def storeProc("Update SAM tables"):
+		table = 'SAM_inf_patient'
