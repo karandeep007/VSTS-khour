51f463b83fc62fad21d0cf5a45e0a49b42787d64
Karandeep Singh <karan.redrock@gmail.com>
2019-03-27 03:33:06 +0530

    delete SAM pipelines

--- SAM pipelines.py
+++
@@ -1,8 +0,0 @@
-## SAM Ingestion piplines to move data from SAMAPPMANAGER into AZure BLOB
-def pipeline("CopySAMData"):
-	def createLinkedService("SAMApp"):
-		connection strin = '10.0.0.11'
-
-def pipeline("SAMInfustionAPP"):
-	def storeProc("Update SAM tables"):
-		table = 'SAM_inf_patient'
